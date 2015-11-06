d18b52cfd35823e0d278599b45cfd3b7c33b9df2
Rachid Laoues <rachid.laoues@orange.com>
2015-11-06 09:36:18 +0100

    Rename POM

--- FlexOfficeWebServer/flexoffice-business/src/test/java/com/orange/flexoffice/business/userTest.java
+++ FlexOfficeWebServer/flexoffice-business/src/test/java/com/orange/flexoffice/business/userTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class userTest {
 
 	@Before
