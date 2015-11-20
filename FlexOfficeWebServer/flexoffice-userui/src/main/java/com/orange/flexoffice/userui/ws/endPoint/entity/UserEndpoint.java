193509eb466723f61c877ca1217de3f0117af21a
Lionel Fremanteau <lionel.fremanteau@orange.com>
2015-11-20 11:13:43 +0100

    add tests for user APIs for userui

--- FlexOfficeWebServer/flexoffice-userui/src/main/java/com/orange/flexoffice/userui/ws/endPoint/entity/UserEndpoint.java
+++ FlexOfficeWebServer/flexoffice-userui/src/main/java/com/orange/flexoffice/userui/ws/endPoint/entity/UserEndpoint.java
@@ -1,24 +1,15 @@
 package com.orange.flexoffice.userui.ws.endPoint.entity;
 
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBElement;
 
-import static com.orange.flexoffice.userui.ws.ParamsConst.USER_ID_PARAM;
 import static com.orange.flexoffice.userui.ws.PathConst.USERS_PATH;
 import static com.orange.flexoffice.userui.ws.PathConst.USER_ID_PATH;
 
-import com.orange.flexoffice.business.common.exception.DataAlreadyExistsException;
-import com.orange.flexoffice.userui.ws.model.XmlUser;
+import com.orange.flexoffice.userui.ws.model.UserSummary;
 
 /**
  * Defines all operations available for a resource "user".
@@ -38,59 +29,16 @@ public interface UserEndpoint {
 	 */
 	@GET
 	@Path(USER_ID_PATH)
-	@Consumes (MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	XmlUser getUser(@PathParam(USER_ID_PARAM) String userId);
+	UserSummary getUserCurrent();
 
-	/**
-	 * Add a new user.
-	 * 
-	 * @param user
-	 * 			  the new user
-	 * @return If ok, a <code>Response</code> with a status code 201 Create & Location.
-	 * 
-	 * @see XmlUser
-	 * @see Response
-	 */
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	XmlUser addUser(XmlUser user) throws DataAlreadyExistsException;
-	
-	/**
-	 * Modifies a specific user.
-	 * To identify a user, a user id is required.
-	 * 
-	 * @param id
-	 * 			  a user id. 
-	 * @param user
-	 *            the new <code>user</code>
-	 * @return If ok, a <code>Response</code> with a status code 201 Create & Location.
-	 * 
-	 * @see XmlUser
-	 * @see Response
-	 */
-	@PUT
-	@Path(USER_ID_PATH)
-	@Consumes(MediaType.APPLICATION_JSON)
-	Response updateUser(@PathParam(USER_ID_PARAM)String id, XmlUser user);
 
-	/**
-	 * Deletes a specific user.
-	 * To identify a user, a user id is required. 
+	
+	/** Initialize tests data in DB
 	 * 
-	 * @param id
-	 * 			  a user id. 
-	 * @return If ok, a <code>Response</code> with a status code 204 No Content.
-	 * 
-	 * @see Response
+	 * @return true if successfully done
 	 */
-	@DELETE
-	@Path(USER_ID_PATH)
-	@Consumes(MediaType.APPLICATION_JSON)
-	Response removeUser(@PathParam(USER_ID_PARAM)String id);
-	
-	
+	boolean executeInitTestFile();
 }
 
 
