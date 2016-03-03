5d7564077c0052acfa6fb90d5754ebffaaa9e6dc
Rachid Laoues <rachid.laoues@orange.com>
2016-03-03 16:01:08 +0100

    check cross domain

--- FlexOfficeWebServer/flexoffice-adminui/src/main/java/com/orange/flexoffice/adminui/ws/utils/ErrorMessageHandler.java
+++ FlexOfficeWebServer/flexoffice-adminui/src/main/java/com/orange/flexoffice/adminui/ws/utils/ErrorMessageHandler.java
@@ -31,7 +31,7 @@ public class ErrorMessageHandler {
 		        //.header("Access-Control-Allow-Origin", "*")
 		        .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
 		        .header("Access-Control-Allow-Credentials", "true")
-		        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
 		        .header("Access-Control-Max-Age", "1209600")
 		        .build();
 	}
