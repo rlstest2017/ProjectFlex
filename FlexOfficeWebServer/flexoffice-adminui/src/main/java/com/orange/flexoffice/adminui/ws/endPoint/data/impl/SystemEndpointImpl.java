bd2f1aa7886a2778a810011e9487aef0f8869acc
Rachid Laoues <rachid.laoues@orange.com>
2016-03-03 16:06:29 +0100

    check cross domain

--- FlexOfficeWebServer/flexoffice-adminui/src/main/java/com/orange/flexoffice/adminui/ws/endPoint/data/impl/SystemEndpointImpl.java
+++ FlexOfficeWebServer/flexoffice-adminui/src/main/java/com/orange/flexoffice/adminui/ws/endPoint/data/impl/SystemEndpointImpl.java
@@ -269,10 +269,10 @@ public class SystemEndpointImpl implements SystemEndpoint {
 	@Override
 	public Response options() {
 	    return Response.ok("")
-	            //.header("Access-Control-Allow-Origin", "*")
+	            .header("Access-Control-Allow-Origin", "*")
 	            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-auth-token")
 	            .header("Access-Control-Allow-Credentials", "true")
-	            //.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+	            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
 	            .header("Access-Control-Allow-Methods", "OPTIONS, HEAD")
 	            .header("Access-Control-Max-Age", "1209600")
 	            .build();
