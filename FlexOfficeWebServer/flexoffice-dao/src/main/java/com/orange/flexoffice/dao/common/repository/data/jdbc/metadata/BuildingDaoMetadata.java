32e883d4f78d2392fa9c96d05d15b63137c61422
Rachid Laoues <rachid.laoues@orange.com>
2016-03-04 15:42:31 +0100

    add test case for addRegion() method

---
+++ FlexOfficeWebServer/flexoffice-dao/src/main/java/com/orange/flexoffice/dao/common/repository/data/jdbc/metadata/BuildingDaoMetadata.java
@@ -0,0 +1,18 @@
+package com.orange.flexoffice.dao.common.repository.data.jdbc.metadata;
+
+/**
+ * BuildingDaoMetadata
+ * @author oab
+ *
+ */
+public final class BuildingDaoMetadata {
+	
+	public static final String BUILDING_TABLE_NAME = "buildings";
+	public static final String BUILDING_ID_COL = "id";
+	public static final String BUILDING_NAME_COL = "name";
+	
+	private BuildingDaoMetadata(){
+		
+	}
+
+}
