94f4e76fb33c6dd0685b653169cadac5e71a8ad5
Rachid Laoues <rachid.laoues@orange.com>
2016-03-10 15:42:09 +0100

    add location userUI in progress

--- FlexOfficeWebServer/flexoffice-userui/src/main/java/com/orange/flexoffice/userui/ws/endPoint/data/impl/LocationEndpointImpl.java
+++ FlexOfficeWebServer/flexoffice-userui/src/main/java/com/orange/flexoffice/userui/ws/endPoint/data/impl/LocationEndpointImpl.java
@@ -1,5 +1,6 @@
 package com.orange.flexoffice.userui.ws.endPoint.data.impl;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,7 +12,11 @@ import com.orange.flexoffice.business.common.service.data.BuildingManager;
 import com.orange.flexoffice.business.common.service.data.CityManager;
 import com.orange.flexoffice.business.common.service.data.CountryManager;
 import com.orange.flexoffice.business.common.service.data.RegionManager;
+import com.orange.flexoffice.business.common.service.data.TestManager;
+import com.orange.flexoffice.dao.common.model.data.BuildingDao;
+import com.orange.flexoffice.dao.common.model.data.CityDao;
 import com.orange.flexoffice.dao.common.model.data.CountryDao;
+import com.orange.flexoffice.dao.common.model.data.RegionDao;
 import com.orange.flexoffice.userui.ws.endPoint.data.LocationEndpoint;
 import com.orange.flexoffice.userui.ws.model.BuildingItem;
 import com.orange.flexoffice.userui.ws.model.LocationItem;
@@ -28,6 +33,8 @@ public class LocationEndpointImpl implements LocationEndpoint {
 	private final ObjectFactory factory = new ObjectFactory();
 	
 	@Autowired
+	private TestManager testManager;
+	@Autowired
 	private CountryManager countryManager;
 	@Autowired
 	private RegionManager regionManager;
@@ -39,7 +46,7 @@ public class LocationEndpointImpl implements LocationEndpoint {
 	@Override
 	public List<LocationItem> getCountries() { // only have rooms !!
 		LOGGER.debug( "Begin call LocationEndpointImpl.getCountries at: " + new Date() );
-		List<CountryDao> dataList = countryManager.findAllCountries(); //TODO change !!!
+		List<CountryDao> dataList = countryManager.findCountries(true); // TODO request from UserUI isFromAdminUI=false
 		List<LocationItem> countryList = new ArrayList<LocationItem>();
 		for (CountryDao countryDao : dataList) {
 			LocationItem country = factory.createLocationItem();
@@ -55,16 +62,14 @@ public class LocationEndpointImpl implements LocationEndpoint {
 	@Override
 	public List<LocationItem> getRegions(String countryId) { // only have rooms !!
 		LOGGER.debug( "Begin call LocationEndpointImpl.getRegions at: " + new Date() );
-		//List<RegionSummaryDto> dataList = regionManager.findAllRegions();
-		// TODO only have rooms !!
+		List<RegionDao> dataList = regionManager.findRegionsByCountryId(countryId, true); // TODO request from UserUI isFromAdminUI=false
 		List<LocationItem> regionList = new ArrayList<LocationItem>();
-//		for (RegionSummaryDto regionDto : dataList) {
-//			RegionSummary region = factory.createRegionSummary();
-//			region.setId(regionDto.getId().toString());
-//			region.setName(regionDto.getName());
-//			region.setCountryName(regionDto.getCountryName());
-//			regionList.add(region);
-//		}
+		for (RegionDao regionDao : dataList) {
+			LocationItem region = factory.createLocationItem();
+			region.setId(regionDao.getId().toString());
+			region.setName(regionDao.getName());
+			regionList.add(region);
+		}
 		LOGGER.debug("List of regions have rooms : nb = " + regionList.size());
 		LOGGER.debug( "End call LocationEndpointImpl.getRegions  at: " + new Date() );
 		return regionList;
@@ -73,17 +78,14 @@ public class LocationEndpointImpl implements LocationEndpoint {
 	@Override
 	public List<LocationItem> getCities(String regionId) { // only have rooms !!
 		LOGGER.debug( "Begin call LocationEndpointImpl.getCities at: " + new Date() );
-		//List<CitySummaryDto> dataList = cityManager.findAllCities();
-		// TODO only have rooms !!
+		List<CityDao> dataList = cityManager.findCitiesByRegionId(regionId, true); // TODO request from UserUI isFromAdminUI=false
 		List<LocationItem> cityList = new ArrayList<LocationItem>();
-//		for (CitySummaryDto cityDto : dataList) {
-//			CitySummary city = factory.createCitySummary();
-//			city.setId(cityDto.getId().toString());
-//			city.setName(cityDto.getName());
-//			city.setCountryName(cityDto.getCountryName());
-//			city.setRegionName(cityDto.getRegionName());
-//			cityList.add(city);
-//		}
+		for (CityDao cityDto : dataList) {
+			LocationItem city = factory.createLocationItem();
+			city.setId(cityDto.getId().toString());
+			city.setName(cityDto.getName());
+			cityList.add(city);
+		}
 		LOGGER.debug("List of cities have rooms : nb = " + cityList.size());
 		LOGGER.debug( "End call LocationEndpointImpl.getCities  at: " + new Date() );
 		return cityList;
@@ -92,23 +94,23 @@ public class LocationEndpointImpl implements LocationEndpoint {
 	@Override
 	public List<BuildingItem> getBuildings(String cityId) { // only have rooms !!
 		LOGGER.debug( "Begin call LocationEndpointImpl.getBuildings at: " + new Date() );
-		//List<BuildingSummaryDto> dataList = buildingManager.findAllBuildings();
-		// TODO only have rooms !!
+		List<BuildingDao> dataList = buildingManager.findBuildingsByCityId(cityId, true); // TODO request from UserUI isFromAdminUI=false
 		List<BuildingItem> buildingList = new ArrayList<BuildingItem>();
-//		for (BuildingSummaryDto buildingDto : dataList) {
-//			BuildingSummary building = factory.createBuildingSummary();
-//			building.setId(buildingDto.getId().toString());
-//			building.setName(buildingDto.getName());
-//			building.setAddress(buildingDto.getAddress());
-//			building.setCountryName(buildingDto.getCountryName());
-//			building.setRegionName(buildingDto.getRegionName());
-//			building.setCityName(buildingDto.getCityName());
-//			building.setNbFloors(BigInteger.valueOf(buildingDto.getNbFloors()));
-//			buildingList.add(building);
-//		}
+		for (BuildingDao buildingDto : dataList) {
+			BuildingItem building = factory.createBuildingItem();
+			building.setBuildingId(buildingDto.getId().toString());
+			building.setBuildingName(buildingDto.getName());
+			building.setNbFloors(BigInteger.valueOf(buildingDto.getNbFloors()));
+			buildingList.add(building);
+		}
 		LOGGER.debug("List of buildings have rooms: nb = " + buildingList.size());
 		LOGGER.debug( "End call LocationEndpointImpl.getBuildings  at: " + new Date() );
 		return buildingList;
+	}
+
+	@Override
+	public boolean executeInitTestFile() {
+		return testManager.executeInitTestFile();
 	}
 
 	
