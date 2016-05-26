e2e29c1983e9987a2b3ebcd9057704a8661184b2
Rachid Laoues <rachid.laoues@orange.com>
2016-05-26 17:33:51 +0200

    Gui Api Server MeetingRoom in progress

--- MeetingRoomWebServer/meetingroom/meetingroom-business/src/main/java/com/orange/meetingroom/business/connector/utils/MeetingRoomInfoTools.java
+++ MeetingRoomWebServer/meetingroom/meetingroom-business/src/main/java/com/orange/meetingroom/business/connector/utils/MeetingRoomInfoTools.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.orange.meetingroom.connector.exception.MeetingRoomInternalServerException;
+import com.orange.meetingroom.connector.exception.MethodNotAllowedException;
+import com.orange.meetingroom.connector.exception.PhpInternalServerException;
 import com.orange.meetingroom.connector.flexoffice.enums.EnumMeetingRoomStatus;
 import com.orange.meetingroom.connector.flexoffice.model.request.MeetingRoomData;
+import com.orange.meetingroom.connector.php.PhpConnectorClient;
+import com.orange.meetingroom.connector.php.model.request.UpdateBookingParameters;
 import com.orange.meetingroom.connector.php.model.response.BookingConnectorReturn;
 import com.orange.meetingroom.connector.php.model.response.MeetingRoomConnectorReturn;
 
@@ -19,11 +25,13 @@ import com.orange.meetingroom.connector.php.model.response.MeetingRoomConnectorR
 public class MeetingRoomInfoTools {
 	
 	private static final Logger LOGGER = Logger.getLogger(MeetingRoomInfoTools.class);
-	
+	static final String FORMAT_JSON = "json";
+	@Autowired
+	PhpConnectorClient phpConnector;
 	@Autowired
 	DateTools dateTools;
 	// TODO get ackTime from /system request
-	Integer ackTime = 0;
+	Integer ackTime = 2;
 	
 	/**
 	 * processMeetingRoomStatus
@@ -72,8 +80,22 @@ public class MeetingRoomInfoTools {
 				Boolean compareDates = dateTools.isTime1BeforeTime2(book.getStartDate(), currentDate, ackTime);
 				if (compareDates) { // startDate + ackTime and currentDate of a booking
 					// exceeding ack Time
-					data.setMeetingRoomStatus(EnumMeetingRoomStatus.FREE);
-					// TODO cancel book
+					try {
+						// cancel book
+						UpdateBookingParameters params = new UpdateBookingParameters(); 
+						params.setRoomID(meetingRoomExternalId);
+						params.setIdReservation(book.getIdReservation());
+						params.setRevisionReservation(book.getRevisionReservation());
+						params.setEndDate(currentDate.toString()); // only endDate to cancel request !!!
+						params.setFormat(FORMAT_JSON);
+						phpConnector.updateBooking(params);
+						data.setMeetingRoomStatus(EnumMeetingRoomStatus.FREE);
+					} catch (MeetingRoomInternalServerException | MethodNotAllowedException	| PhpInternalServerException e) {
+						LOGGER.debug("Exception when trying to cancel reservation, with message:" + e.getMessage());
+						// keep it in ack status
+						data.setMeetingRoomStatus(EnumMeetingRoomStatus.ACK);
+						setMeetingRoomData(book, data, meetingRoomExternalId);
+					}
 				} else {
 					// still in ack time
 					data.setMeetingRoomStatus(EnumMeetingRoomStatus.ACK);
