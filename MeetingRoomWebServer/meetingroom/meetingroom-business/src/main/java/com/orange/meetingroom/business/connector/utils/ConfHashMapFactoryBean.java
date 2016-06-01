4953cdbc1e8cf3bf3f99eb49c9e086d0bae7cac7
Rachid Laoues <rachid.laoues@orange.com>
2016-06-01 14:53:55 +0200

    GUI server developp progress

--- MeetingRoomWebServer/meetingroom/meetingroom-business/src/main/java/com/orange/meetingroom/business/connector/utils/ConfHashMapFactoryBean.java
+++ MeetingRoomWebServer/meetingroom/meetingroom-business/src/main/java/com/orange/meetingroom/business/connector/utils/ConfHashMapFactoryBean.java
@@ -3,6 +3,8 @@ package com.orange.meetingroom.business.connector.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,26 +30,31 @@ public class ConfHashMapFactoryBean implements FactoryBean<Map<String, Integer>>
 	@Autowired
 	DateTools dateTools;
 	
+	public Map<String, Integer> configMap = new HashMap<String, Integer>();
+	
+	@PostConstruct
+	public void initialize() {
+		try {
+			SystemConnectorReturn systemreturn;
+			systemreturn = flexofficeConnector.getSystem();
+			
+			// process startDate
+			Integer startDate = 0;
+			Integer nbSeconds = systemreturn.getDashboardStartDate();
+			if (nbSeconds != 0) {
+				startDate = dateTools.processStartDate(nbSeconds);
+			}
+			
+			configMap.put(EnumSystemInMap.ACK_TIME.toString(), systemreturn.getAckTime());
+			configMap.put(EnumSystemInMap.DASHBOARD_START_DATE.toString(), startDate);
+			configMap.put(EnumSystemInMap.DASHBOARD_MAX_BOOKINGS.toString(), systemreturn.getDashboardMaxBookings());
+			} catch (FlexOfficeInternalServerException | MeetingRoomInternalServerException e) {
+			LOGGER.debug("Exception thrown in flexofficeConnector.getSystem() in ConfHashMapFactoryBean class: " + e.getMessage(), e);
+			}
+	}
+	
 	@Override
 	public Map<String, Integer> getObject()  {
-		Map<String, Integer> configMap = new HashMap<String, Integer>();
-		try {
-		SystemConnectorReturn systemreturn;
-		systemreturn = flexofficeConnector.getSystem();
-		
-		// process startDate
-		Integer startDate = 0;
-		Integer nbSeconds = systemreturn.getDashboardStartDate();
-		if (nbSeconds != 0) {
-			startDate = dateTools.processStartDate(nbSeconds);
-		}
-		
-		configMap.put(EnumSystemInMap.ACK_TIME.toString(), systemreturn.getAckTime());
-		configMap.put(EnumSystemInMap.DASHBOARD_START_DATE.toString(), startDate);
-		configMap.put(EnumSystemInMap.DASHBOARD_MAX_BOOKINGS.toString(), systemreturn.getDashboardMaxBookings());
-		} catch (FlexOfficeInternalServerException | MeetingRoomInternalServerException e) {
-		LOGGER.debug("Exception thrown in flexofficeConnector.getSystem() in ConfHashMapFactoryBean class: " + e.getMessage(), e);
-		}
 		return configMap;
 	}
 
