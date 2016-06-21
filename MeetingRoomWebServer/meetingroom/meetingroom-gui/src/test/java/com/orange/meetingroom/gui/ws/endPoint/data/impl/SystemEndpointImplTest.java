b13983a8f6b8ff51ed1b29e86ee899e2f78872cd
Rachid Laoues <rachid.laoues@orange.com>
2016-06-21 11:25:13 +0200

    add tests

--- MeetingRoomWebServer/meetingroom/meetingroom-gui/src/test/java/com/orange/meetingroom/gui/ws/endPoint/data/impl/SystemEndpointImplTest.java
+++ MeetingRoomWebServer/meetingroom/meetingroom-gui/src/test/java/com/orange/meetingroom/gui/ws/endPoint/data/impl/SystemEndpointImplTest.java
@@ -1,6 +1,7 @@
 package com.orange.meetingroom.gui.ws.endPoint.data.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.FileNotFoundException;
 
@@ -10,10 +11,12 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.util.Log4jConfigurer;
 
 import com.orange.meetingroom.gui.ws.endPoint.data.SystemEndpoint;
 import com.orange.meetingroom.gui.ws.model.SystemCurrentDate;
+import com.orange.meetingroom.gui.ws.model.SystemRemoteMacAddress;
 import com.orange.meetingroom.gui.ws.model.SystemReturn;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -74,4 +77,22 @@ public class SystemEndpointImplTest {
 		assertEquals(false, expectedResult);
 	}
 
+	@Test
+	public void TestC() {
+		// SetUp
+		boolean expectedResult = false;
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setRemoteAddr("192.168.142.219");
+		
+		try {
+			SystemRemoteMacAddress returnedData = systemEndpoint.getSystemRemoteMacAddress(request);
+			assertNotEquals(null , returnedData);
+			
+		} catch (Exception e) {
+			expectedResult = true;
+			LOGGER.error(e.getMessage());
+		}	
+		// Asserts
+		assertEquals(false, expectedResult);
+	}
 }
