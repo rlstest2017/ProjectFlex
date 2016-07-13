a19a5e71c17808f8c0a3b0097b1814752ba2df35
Rachid Laoues <rachid.laoues@orange.com>
2016-07-13 17:05:32 +0200

    add SQL script for V2.1

--- FlexOfficeWebServer/flexoffice-dao/src/main/resources/sql/init_db_flexoffice.sql
+++ FlexOfficeWebServer/flexoffice-dao/src/main/resources/sql/init_db_flexoffice.sql
@@ -1,11 +1,11 @@
 
-DROP TABLE IF EXISTS users, gateways, rooms, sensors, alerts, room_stats, room_daily_occupancy, teachin_sensors, configuration, preferences, buildings, cities, regions, countries;
-DROP TABLE IF EXISTS meetingrooms, agents, dashboards, meetingroom_groups_configuration, meetingroom_stats, meetingroom_daily_occupancy;
+DROP TABLE IF EXISTS users, gateways, rooms, sensors, alerts, room_stats, room_daily_occupancy, teachin_sensors, configuration, preferences, buildings, cities, regions, countries CASCADE;
+DROP TABLE IF EXISTS meetingrooms, agents, dashboards, meetingroom_groups_configuration, meetingroom_stats, meetingroom_daily_occupancy CASCADE;
    
 DROP TYPE IF EXISTS meetingroomStatus, meetingroomType, agentStatus, dashboardStatus, meetingroomInfo; 
 DROP TYPE IF EXISTS userRole, deviceType, sensorType, sensorStatus, gatewayStatus, roomStatus, roomType, sensorOccupancyInfo, roomInfo, teachinStatus, sensorTeachinStatus, commandModel;     
 
-DROP TABLE IF EXISTS extra_propertie_enum_def, extra_propertie_number_def, extra_propertie_enum_values, product_def, rooms_extra_propertie_values, meetingrooms_extra_propertie_values;
+DROP TABLE IF EXISTS extra_property_def, extra_property_enum_values, product_def, rooms_extra_property_values, meetingrooms_extra_property_values;
 
 --EXTENSION
 CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
@@ -235,42 +235,34 @@ CREATE TABLE product_def (
     description text
 );
 
-CREATE TABLE extra_propertie_enum_def (
+CREATE TABLE extra_property_def (
     id serial NOT NULL,
     name citext unique NOT NULL,
     product_def_id integer NOT NULL,
-    description text
-);
-
-CREATE TABLE extra_propertie_number_def (
-    id serial NOT NULL,
-    name citext unique NOT NULL,
-    product_def_id integer NOT NULL,
+    is_number boolean DEFAULT false,
     number_min_value real,
     number_max_value real,
     description text
 );
 
--- values of enums defined in extra_propertie_enum_names
-CREATE TABLE extra_propertie_enum_values (
+-- values of enums defined in extra_property_def
+CREATE TABLE extra_property_enum_values (
     id serial NOT NULL,
     enum_value citext unique NOT NULL,
-    enum_def_id integer NOT NULL
+    property_def_id integer NOT NULL
 );
 
-CREATE TABLE rooms_extra_propertie_values (
+CREATE TABLE rooms_extra_property_values (
     id serial NOT NULL,
-    enum_def_id integer,
-    number_def_id integer,
+    property_def_id integer NOT NULL,
     room_id integer,
     enum_value_id integer,
     number_value real
 );
 
-CREATE TABLE meetingrooms_extra_propertie_values (
+CREATE TABLE meetingrooms_extra_property_values (
     id serial NOT NULL,
-    enum_def_id integer,
-    number_def_id integer,
+    property_def_id integer NOT NULL,
     meetingroom_id integer,
     enum_value_id integer,
     number_value real
@@ -376,35 +368,27 @@ ALTER TABLE ONLY meetingroom_daily_occupancy
 ALTER TABLE ONLY product_def
     ADD CONSTRAINT product_def_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY extra_propertie_enum_def
-    ADD CONSTRAINT extra_propertie_enum_def_pkey PRIMARY KEY (id),
+ALTER TABLE ONLY extra_property_def
+    ADD CONSTRAINT extra_property_def_pkey PRIMARY KEY (id),
     ADD FOREIGN KEY(product_def_id) REFERENCES product_def(id),
     ADD CONSTRAINT name_length_check CHECK (char_length(name) <= 50);
 
-ALTER TABLE ONLY extra_propertie_number_def
-    ADD CONSTRAINT extra_propertie_number_def_pkey PRIMARY KEY (id),
-    ADD FOREIGN KEY(product_def_id) REFERENCES product_def(id),
-    ADD CONSTRAINT name_length_check CHECK (char_length(name) <= 50);
-
-ALTER TABLE ONLY extra_propertie_enum_values
-    ADD CONSTRAINT extra_propertie_enum_values_pkey PRIMARY KEY (id),
-    ADD FOREIGN KEY(enum_def_id) REFERENCES extra_propertie_enum_def(id),
+ALTER TABLE ONLY extra_property_enum_values
+    ADD CONSTRAINT extra_property_enum_values_pkey PRIMARY KEY (id),
+    ADD FOREIGN KEY(property_def_id) REFERENCES extra_property_def(id),
     ADD CONSTRAINT enum_value_length_check CHECK (char_length(enum_value) <= 50);
 
-ALTER TABLE ONLY rooms_extra_propertie_values
-    ADD CONSTRAINT rooms_extra_propertie_values_pkey PRIMARY KEY (id),
-    ADD FOREIGN KEY(enum_def_id) REFERENCES extra_propertie_enum_def(id),
-	ADD FOREIGN KEY(number_def_id) REFERENCES extra_propertie_number_def(id),
-    ADD FOREIGN KEY(room_id) REFERENCES rooms(id),
-    ADD FOREIGN KEY(enum_value_id) REFERENCES extra_propertie_enum_values(id);
+ALTER TABLE ONLY rooms_extra_property_values
+    ADD CONSTRAINT rooms_extra_property_values_pkey PRIMARY KEY (id),
+    ADD FOREIGN KEY(property_def_id) REFERENCES extra_property_def(id),
+	ADD FOREIGN KEY(room_id) REFERENCES rooms(id),
+    ADD FOREIGN KEY(enum_value_id) REFERENCES extra_property_enum_values(id);
     
-ALTER TABLE ONLY meetingrooms_extra_propertie_values
-    ADD CONSTRAINT meetingrooms_extra_propertie_values_pkey PRIMARY KEY (id),
-    ADD FOREIGN KEY(enum_def_id) REFERENCES extra_propertie_enum_def(id),
-	ADD FOREIGN KEY(number_def_id) REFERENCES extra_propertie_number_def(id),
+ALTER TABLE ONLY meetingrooms_extra_property_values
+    ADD CONSTRAINT meetingrooms_extra_property_values_pkey PRIMARY KEY (id),
+    ADD FOREIGN KEY(property_def_id) REFERENCES extra_property_def(id),
     ADD FOREIGN KEY(meetingroom_id) REFERENCES meetingrooms(id),
-    ADD FOREIGN KEY(enum_value_id) REFERENCES extra_propertie_enum_values(id);
-
+    ADD FOREIGN KEY(enum_value_id) REFERENCES extra_property_enum_values(id);
     
 CREATE INDEX users_last_name_idx ON users USING btree (last_name);
 CREATE INDEX users_email_idx ON users USING btree (email);
@@ -480,3 +464,4 @@ INSERT INTO product_def (name, description) VALUES
 		('ALL', 'corresponding extra properties are associated to all products'),
 		('ROOM', 'corresponding extra properties are associated to non bookable rooms'),
 		('MEETINGROOM', 'corresponding extra properties are associated to bookable rooms');
+    
